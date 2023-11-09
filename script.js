8d74182fd0df912715d81be2a21dc5429cb68120
Aayush895 <aayush895@gmail.com>
2023-11-09 21:24:21 +0530

    Final Changes done

---
+++ script.js
@@ -0,0 +1,31 @@
+const submitBtn = document
+  .querySelector('.form-validation')
+  .querySelector('button')
+const input = document.querySelector('#input-email')
+const form = document.querySelector('.form-validation')
+
+function validateEmail(email) {
+  const atSign = email.indexOf('@')
+  const dotSign = email.indexOf('.')
+  if (atSign < 1 || dotSign < atSign + 2 || dotSign + 2 >= email.length) {
+    document.querySelector(
+      '.validity-message'
+    ).innerText = `Invalid email, please try again!`
+    document.querySelector('.validity-message').classList.remove('success')
+    document.querySelector('.validity-message').classList.add('error')
+  } else {
+    document.querySelector(
+      '.validity-message'
+    ).innerText = `Form Submission Successful!`
+    document.querySelector('.validity-message').classList.remove('error')
+    document.querySelector('.validity-message').classList.add('success')
+  }
+
+  setTimeout(() => {
+    document.querySelector('.validity-message').innerText = ''
+  }, 2000)
+
+  input.value = ''
+}
+
+submitBtn.addEventListener('click', () => validateEmail(input.value))
